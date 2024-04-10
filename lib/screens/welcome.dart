357ffda5efe38459af2f39be2ee02fb1557563dc
SIVANANDHA MS <72291646+Sivanandha02@users.noreply.github.com>
2024-04-10 16:44:46 +0530

    Update welcome.dart

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,5 +1,10 @@
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_3_ui/screens/Sign.dart';
+import 'package:flutter_3_ui/screens/login.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class WelCome extends StatelessWidget {
@@ -10,20 +15,61 @@ class WelCome extends StatelessWidget {
         decoration: BoxDecoration(
           image: DecorationImage(
               colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                  Colors.black.withOpacity(0.4), BlendMode.dstOut),
               fit: BoxFit.cover,
               image: NetworkImage(
                   "https://images.unsplash.com/photo-1491147334573-44cbb4602074?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8f")),
         ),
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Text(" The best\n app for\n your plants",
-              style: GoogleFonts.roboto(
-                  fontSize: 60,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold)),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(" The best\n app for\n your plants",
+                  style: GoogleFonts.roboto(
+                      fontSize: 60,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: ((context) => SignIn())));
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 50)),
+              child: Text(
+                "Sign up",
+                style: GoogleFonts.montserrat(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: ((context) => LogIn())));
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 74, 136, 77),
+                  minimumSize: Size(double.infinity, 50)),
+              child: Text(
+                "Login",
+                style: GoogleFonts.montserrat(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
